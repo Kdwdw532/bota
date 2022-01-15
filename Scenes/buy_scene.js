296b49463e057900b498ca9e537126260489f7e8
Kdwdw532 <kmironov1993@gmail.com>
2022-01-15 23:57:10 +0300

    Initial commit

---
+++ Scenes/buy_scene.js
@@ -0,0 +1,29 @@
+const {Telegraf ,Scenes: {WizardScene}, Markup} = require('telegraf');
+const config = require('config');
+
+const pay = Telegraf.on('text', async (ctx) => {
+  if (ctx.update.message.text.split('?')[0] === 'https://telegram.me/BTC_CHANGE_BOT') {
+    ctx.telegram.sendMessage(config.get('admin'), `
+    ${ctx.message.text}
+Йоу, админ, тут @${ctx.session.order.buyer} купил ${ctx.session.order.item}. Чек выше
+    `);
+    ctx.reply(`Теперь ты можешь написать @alestratovish в лс и забрать свой товар`, Markup.inlineKeyboard([
+      Markup.button.callback('Вернуться в меню', 'menu'),
+      Markup.button.url('Написать админу', 'https://t.me/alestratovish'),
+    ]));
+    ctx.scene.leave();
+  } else {
+    ctx.reply('Давай сюда чек, врунишка', Markup.inlineKeyboard([
+      Markup.button.callback('Отменить покупку', 'menu')
+    ]));
+  }
+});
+
+
+const buyScene = new WizardScene('buyScene', pay);
+
+buyScene.enter(ctx => ctx.editMessageText(`
+Для того, что бы оплатить товар - пришлите BTC чек боту
+`));
+
+module.exports = buyScene;
